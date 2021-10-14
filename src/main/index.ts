fa16c342441033e1e0cd564c7211f48bbfc7363c
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-14 19:15:00 -0300

    feat: solve the challenge with poor code

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,17 +1,46 @@
 import './config/module-alias'
 import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import validator from 'validator'
 
 import { env } from '@/main/config/env'
 import 'reflect-metadata'
 
-const http = require('http')
-const hostname = '127.0.0.1'
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World')
-})
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-server.listen(env.port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${env.port}/`)
+const options = {
+  minLength: 9,
+  minLowercase: 1,
+  minUppercase: 1,
+  minNumbers: 1,
+  minSymbols: 1,
+  returnScore: true,
+  pointsPerUnique: 0,
+  pointsPerRepeat: 1,
+  pointsForContainingLower: 0,
+  pointsForContainingUpper: 0,
+  pointsForContainingNumber: 0,
+  pointsForContainingSymbol: 0
+}
+const blacklistRegex = /[_|~=`{}[\]:";'<>?,./ ]/g
+
+app.post('/', (req, res) => {
+  const { password } = req.body
+  const hasBlacklistSymbol = password.match(blacklistRegex)
+  if (hasBlacklistSymbol) {
+    res.send(false)
+    return
+  }
+  const points = validator.isStrongPassword(password, options) as unknown
+  if (password.length < 9 || points as number > 0) {
+    res.send(false)
+    return
+  }
+  res.send(true)
+})
+app.listen(env.port, () => {
+  console.log(`Server running at http://localhost:${env.port}/`)
 })
