5ad88d6497060ac08459b5f25a5439359bb94d4c
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-15 13:12:28 -0300

    impr: ensure password has only unique chars

--- src/domain/use-cases/password-validation.ts
+++ src/domain/use-cases/password-validation.ts
@@ -6,8 +6,10 @@ type Setup = (validator: PasswordValidator) => PasswordValidation
 
 export const setupPasswordValidation: Setup = validator => ({ password }) => {
   const blacklistRegex = /[_|~=`{}[\]:";'<>?,./ ]/g
+  const uniqueRegex = /^(?:([A-Za-z0-9!@#$%^&*()-+])(?!.*\1))*$/g
   const hasBlacklistSymbol = password.match(blacklistRegex)
-  if (hasBlacklistSymbol || password.length < 9) {
+  const hasOnlyUniqueChars = password.match(uniqueRegex)
+  if (hasBlacklistSymbol || password.length < 9 || !hasOnlyUniqueChars) {
     return false
   }
   return validator.isValid({ password })
