5d59947c33684e803233ce616a6954d64d8b0e5f
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-15 06:37:10 -0300

    test: ensure ValidationController returns bad request error

--- tests/application/controllers/validation.spec.ts
+++ tests/application/controllers/validation.spec.ts
@@ -19,9 +19,19 @@ describe('ValidationController', () => {
     const httpResponse = await sut.handle({ password: undefined })
 
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.data).toEqual(new Error('Bad Request'))
   })
 
   it('should return 400 if httpRequest is null', async () => {
+    const sut = new ValidationController()
+
+    const httpResponse = await sut.handle({ password: null })
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.data).toEqual(new Error('Bad Request'))
+  })
+
+  it('should return 200 if httpRequest is null', async () => {
     const sut = new ValidationController()
 
     const httpResponse = await sut.handle({ password: null })
