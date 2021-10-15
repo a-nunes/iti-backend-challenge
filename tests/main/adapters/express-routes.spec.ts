2104bff946fd5bb9a50f704c41bd097ab7193f83
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-10-15 19:03:44 -0300

    refactor: isolate sut creation

--- tests/main/adapters/express-routes.spec.ts
+++ tests/main/adapters/express-routes.spec.ts
@@ -1,8 +1,8 @@
 import { Controller } from '@/application/controllers'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { getMockReq, getMockRes } from '@jest-mock/express'
-import { RequestHandler } from 'express'
+import { RequestHandler, Response, Request, NextFunction } from 'express'
 
 export const adaptExpressRouter = (controller: Controller): RequestHandler => {
   return (req, res) => {
@@ -11,14 +11,24 @@ export const adaptExpressRouter = (controller: Controller): RequestHandler => {
 }
 
 describe('ExpressRouterAdapter', () => {
+  let req: Request
+  let res: Response
+  let next: NextFunction
+  let controller: MockProxy<Controller>
+  let sut: RequestHandler
+
+  beforeAll(() => {
+    req = getMockReq({ body: { data: 'any_data' } })
+    res = getMockRes().res
+    next = getMockRes().next
+    controller = mock()
+  })
+
+  beforeEach(() => {
+    sut = adaptExpressRouter(controller)
+  })
+
   it('should call handle with correct params', async () => {
-    const req = getMockReq({ body: { data: 'any_data' } })
-    const res = getMockRes().res
-    const next = getMockRes().next
-    const controller = mock<Controller>()
-
-    const sut = adaptExpressRouter(controller)
-
     await sut(req, res, next)
 
     expect(controller.handle).toHaveBeenCalledWith({ data: 'any_data' })
